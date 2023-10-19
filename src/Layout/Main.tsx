24aa7641ea61e84b72bfe64472ca1b12332c8c5d
Shakil57375 <shakil57375@gmail.com>
2023-10-19 22:49:53 +0600

    scroll to top button added

--- src/Layout/Main.tsx
+++ src/Layout/Main.tsx
@@ -1,11 +1,13 @@
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 const Main = () => {
   return (
     <div className="w-full bg-black">
       <Navbar />
       <Outlet />
+      <ScrollToTop/>
       <Footer />
     </div>
   );
