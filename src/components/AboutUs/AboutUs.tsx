cd9cd51ec88f4e3d2c61bd0ee8fbc0121f47cdce
Shakil57375 <shakil57375@gmail.com>
2023-10-31 22:59:42 +0600

    AOS animation added

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,7 +1,7 @@
 const AboutUs = () => {
   return (
     <div className=" mt-20 lg:px-28 px-4 lg:py-6 py-4 text-white space-y-8">
-      <p>Pricing</p>
+      <p>About us</p>
     </div>
   )
 }
--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,6 +1,8 @@
 import Texts from "../Texts/Texts";
 import React, { ReactNode } from "react";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 interface CardProps {
   icon: ReactNode;
   styledTitle: string;
@@ -14,8 +16,13 @@ const Card: React.FC<CardProps> = ({
   styledTitle,
   description,
 }) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+    });
+  }, []);
   return (
-    <div className="flex lg:flex-row transition-all ease-in-out duration-500 hover:-translate-y-1 hover:-translate-x-1 flex-col items-start gap-5 px-10 py-6 bg-gray-950 shadow-2xl group rounded-2xl hover:bg-gradient-to-r from-red-600 to-purple-900">
+    <div data-aos="flip-left" className="flex lg:flex-row transition-all ease-in-out duration-500 hover:-translate-y-1 hover:-translate-x-1 flex-col items-start gap-5 px-10 py-6 bg-gray-950 shadow-2xl group rounded-2xl hover:bg-gradient-to-r from-red-600 to-purple-900">
       <div className="bg-pink-950 p-5 text-4xl rounded-full">{icon}</div>
       <div>
         <Texts
