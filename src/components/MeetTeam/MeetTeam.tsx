c47b5f08baaab2e5d5b37a97588d4dce7471ac93
Shakil57375 <shakil57375@gmail.com>
2023-11-02 14:44:17 +0600

    about us added

--- src/components/MeetTeam/MeetTeam.tsx
+++ src/components/MeetTeam/MeetTeam.tsx
@@ -1,16 +1,24 @@
 import ProfileCard from "../ProfileCard/ProfileCard";
-import { FaFacebook, FaGithub, FaLinkedin, FaLink } from "react-icons/fa";
 const MeetTeam = () => {
   return (
-    <div>
+    <div className="flex justify-around items-center gap-10">
       <ProfileCard
-        imageSrc="https://i.ibb.co/PFvwf3V/356821769-250647931002332-6524631220417449226-n.jpg"
-        facebookIcon={<FaFacebook />}
-        githubIcon={<FaGithub />}
-        linedInIcon={<FaLinkedin />}
-        portfolioIcon={<FaLink />}
+        facebookLink="https://www.facebook.com/profile.php?id=100056182026221&_rdc=1&_rdr"
+        githubLink="https://github.com/Shakil57375"
+        linkedInLink="https://www.linkedin.com/in/shakil-hossian-757817263/"
+        portfolioLink="https://64a01a5550d4bf24857df74a--lucky-cucurucho-81c61e.netlify.app/?fbclid=IwAR2klP83RE9tqHl1xEktuuP300nwLifq4X4meq_CoMzdP6hVQjyIQEZfqAk"
+        imageSrc="https://i.ibb.co/YyYmj91/shakil-img.jpg"
         styledTitle=" Shakil Hossain"
         headerTitle=" Front end developer "
+      />
+      <ProfileCard
+        facebookLink="https://www.facebook.com/suhanurrohoman.sumon"
+        githubLink="https://github.com/Suhanur-Rohoman-Sumon-github"
+        linkedInLink="https://www.linkedin.com/in/suhanur-rohoman-sumon-5b5266237/"
+        portfolioLink="https://celebrated-puffpuff-96980d.netlify.app/?fbclid=IwAR2BTcl43fU39Ld85Z6kIFnAs99ggxU95McuFCNRssyU75vlU6kDuHjWUDg"
+        imageSrc="https://i.ibb.co/NTVvZqB/sumon-img.png"
+        styledTitle="Suhanur Rohoman"
+        headerTitle=" Backend developer"
       />
     </div>
   );
--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,19 +1,19 @@
 import "./profileCard.css";
-import { ReactNode } from "react";
+import { FaFacebook, FaGithub, FaLinkedin, FaLink } from "react-icons/fa";
 interface CardProps {
-  facebookIcon: ReactNode;
-  githubIcon: ReactNode;
-  linedInIcon: ReactNode;
-  portfolioIcon: ReactNode;
+  facebookLink: string;
+  githubLink: string;
+  linkedInLink: string;
+  portfolioLink: string;
   styledTitle: string;
   headerTitle: string;
   imageSrc: string;
 }
 const ProfileCard: React.FC<CardProps> = ({
-  facebookIcon,
-  githubIcon,
-  linedInIcon,
-  portfolioIcon,
+  facebookLink,
+  githubLink,
+  linkedInLink,
+  portfolioLink,
   headerTitle,
   styledTitle,
   imageSrc,
@@ -27,21 +27,29 @@ const ProfileCard: React.FC<CardProps> = ({
       <div className="contents">
         <div className="details">
           <h2>
-            {styledTitle} <br /> <span>{headerTitle}</span>
+            {styledTitle} <br /> <span className="!text-lg">{headerTitle}</span>
           </h2>
-          <div className="actionBtn">
-            <div className="bg-pink-950 p-5 text-4xl rounded-full">
-              {facebookIcon}
-            </div>
-            <div className="bg-pink-950 p-5 text-4xl rounded-full">
-              {githubIcon}
-            </div>
-            <div className="bg-pink-950 p-5 text-4xl rounded-full">
-              {linedInIcon}
-            </div>
-            <div className="bg-pink-950 p-5 text-4xl rounded-full">
-              {portfolioIcon}
-            </div>
+          <div className="actionBtn mt-5">
+            <a className="transition-all ease-in-out duration-500 hover:-translate-y-1 hover:-translate-x-1 " href={facebookLink} target="_blank" rel="noopener noreferrer">
+              <div className="bg-gradient-to-br from-blue-500 via-blue-800 to-blue-900 shadow-lg p-3 text-2xl rounded-full">
+                <FaFacebook />
+              </div>
+            </a>
+            <a className="transition-all ease-in-out duration-500 hover:-translate-y-1 hover:-translate-x-1 " href={githubLink} target="_blank" rel="noopener noreferrer">
+              <div className="bg-gradient-to-br from-gray-700 to-gray-500 shadow-lg p-3 text-2xl rounded-full">
+                <FaGithub />
+              </div>
+            </a>
+            <a className="transition-all ease-in-out duration-500 hover:-translate-y-1 hover:-translate-x-1 " href={linkedInLink} target="_blank" rel="noopener noreferrer">
+              <div className="bg-gradient-to-br from-blue-500 via-blue-800 to-blue-900 shadow-lg p-3 text-2xl rounded-full">
+                <FaLinkedin />
+              </div>
+            </a>
+            <a className="transition-all ease-in-out duration-500 hover:-translate-y-1 hover:-translate-x-1 " href={portfolioLink} target="_blank" rel="noopener noreferrer">
+              <div className="bg-gradient-to-br from-red-600 to-orange-400 shadow-lg p-3 text-2xl rounded-full">
+                <FaLink />
+              </div>
+            </a>
           </div>
         </div>
       </div>
